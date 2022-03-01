888af2645001fb704cce1030f1c9608c13c42b7f
Prabhu <70853875+prabhukiran9999@users.noreply.github.com>
2022-02-28 22:53:45 -0500

    destroying terraform

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "remote" {}
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.9.0"
-    }
-  }
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "3.9.0"
+#     }
+#   }
 }
 
 
