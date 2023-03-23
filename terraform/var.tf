2929db152ac5ab3dac607e499989249a3738b88a
Ryan Loiselle <ryan.loiselle@gov.bc.ca>
2023-03-22 22:33:45 -0700

    updated ami back to ami-07b9af865d56e30ac

--- terraform/var.tf
+++ terraform/var.tf
@@ -23,7 +23,7 @@ variable "lc_name" {
 }
 variable "iamge_id" {
   description = "id of the ami used"
-  default     = "ami-06e39367bd3bb55a7"
+  default     = "ami-07b9af865d56e30ac"
   type        = string
 }
 
